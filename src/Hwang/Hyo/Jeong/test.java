1a79b6cdbbfa688d32188875ce52626dc77e5990
user <user@DESKTOP-RPEB900>
2020-10-14 23:23:18 +0900

    커밋테스트2

--- src/Hwang/Hyo/Jeong/test.java
+++ src/Hwang/Hyo/Jeong/test.java
@@ -4,7 +4,7 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("이클립스 커밋 테스트");
+		System.out.println("이클립스 커밋 테스트2");
 	}
 
 }
